65d6208e25c07f1285ef55705c44f2c04dd89ae4
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-12-11 13:21:01 +0700

    Modules

---
+++ developers.py
@@ -0,0 +1,12 @@
+# modules
+def pengembang():
+  print("")
+  print("===================================================")
+  print("|--\t\tProgram UAP PI \t\t\t--|")
+  print("===================================================")
+  print("")
+  print("----        Selamat Datang Program Kami       ----")
+  print("\tHamzah Hanif - 2117051032\t")
+  print("\tAuvar Mahsa Fahlevi - 2117051027 \t")
+  print("\tRoy Rafles Matorang Pasaribu - 2117051058 \t")
+  print("==================================================")
