83a1fbd04c1494b0292886332c617e994c47d5b0
Auvar Mahsa Fahlevi <92295750+Auvarmf@users.noreply.github.com>
2022-12-11 12:53:05 +0700

    Update ProgramUAP_2117051027_2117051032_2117051058.py

--- ProgramUAP_2117051027_2117051032_2117051058.py
+++ ProgramUAP_2117051027_2117051032_2117051058.py
@@ -5,3 +5,12 @@ import os
 
 katabijak = requests.get('https://katanime.vercel.app/api/getbyanime?anime=naruto&page=1')
 katabijak2 = requests.get('https://katanime.vercel.app/api/getbyanime?anime=bleach&page=1')
+
+katabijak
+katabijak2
+
+katabijak = katabijak.json()
+katabijak2=katabijak2.json()
+
+katabijak['result']
+katabijak2['result']
