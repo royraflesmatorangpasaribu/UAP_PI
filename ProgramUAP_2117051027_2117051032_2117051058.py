d60602c13cf02b643fd3ce10c0c86d2a8825c2d4
Auvar Mahsa Fahlevi <92295750+Auvarmf@users.noreply.github.com>
2022-12-11 12:52:26 +0700

    Update ProgramUAP_2117051027_2117051032_2117051058.py

--- ProgramUAP_2117051027_2117051032_2117051058.py
+++ ProgramUAP_2117051027_2117051032_2117051058.py
@@ -2,3 +2,6 @@ import json, requests
 import time #waktu
 import datetime #tanggal
 import os
+
+katabijak = requests.get('https://katanime.vercel.app/api/getbyanime?anime=naruto&page=1')
+katabijak2 = requests.get('https://katanime.vercel.app/api/getbyanime?anime=bleach&page=1')
